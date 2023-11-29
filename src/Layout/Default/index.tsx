0929b971d23bc31e00b6bced801a5a7e6901ffb7
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-11-29 19:13:29 +0300

    Add simple pages

---
+++ src/Layout/Default/index.tsx
@@ -0,0 +1,18 @@
+export default function Default() {
+	return (
+		<>
+			<button data-theme="cupcake" className="btn btn-primary">
+				Hello daisyUI
+			</button>
+			<button data-theme="dark" className="btn btn-primary">
+				Hello daisyUI
+			</button>
+			<button data-theme="light" className="btn btn-primary">
+				Hello daisyUI
+			</button>
+			<button data-theme="emerald" className="btn btn-primary">
+				Hello daisyUI
+			</button>
+		</>
+	);
+}
